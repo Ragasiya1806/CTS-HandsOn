5e430b31137bc6f2fb03c50291bd92e012a82730
Ragasiya1806 <ragasiyamurugan1806@gmail.com>
2026-06-27 12:36:23 +0530

    Add files via upload

---
+++ Design principles and patterns/Singleton.java
@@ -0,0 +1,34 @@
+class Logger {
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger Instance Created");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First Log Message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second Log Message");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
+    }
+}
